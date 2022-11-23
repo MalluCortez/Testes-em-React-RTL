3dece652e73eeaf6275943adc1b530fa7ee40bab
MalluCortez <maria.santana@outlook.com.br>
2022-11-22 21:16:15 -0300

    requisito 3 feito

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testando o componente <FavoritePokemon.js />', () => {
+  test('Verifica se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    // Acessar
+    renderWithRouter(<FavoritePokemon />);
+    const textNotFavorite = screen.getByText(/no favorite pokémon found/i);
+    expect(textNotFavorite).toBeInTheDocument();
+  });
+});
